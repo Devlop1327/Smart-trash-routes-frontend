1f6c70f7a7dcd9feb73ec2c8d95f96a58c849f0c
Devlop1327 <primevideo1327@gmail.com>
2026-05-06 16:27:42 -0500

    Change - App Name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.smarttrash.routes',
-  appName: 'Smart Trash Routes ',
+  appName: 'ST Ciudadano',
   webDir: 'www',
   plugins: {
     CapacitorHttp: {
